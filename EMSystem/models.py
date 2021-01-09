8fd4e99be667412b864b62b97b75f415a1f836ba
SHU-Yce <1838507591@qq.com>
2021-01-09 14:10:07 +0800

    建表,增加外键约束

--- EMSystem/models.py
+++ EMSystem/models.py
@@ -8,7 +8,7 @@ class S(models.Model):                                                     # 学
     csrq = models.DateField()                                              # 出生日期
     jg = models.CharField(max_length=32)                                   # 籍贯
     sjhm = models.CharField(max_length=32, unique=True)                    # 手机号码
-    yxh = models.CharField(max_length=8)                                   # 院系号
+    yxh = models.ForeignKey('D', to_field='yxh', on_delete=models.CASCADE)      # 院系号(外键)
 
 
 class D(models.Model):                                                     # 院系表
@@ -19,14 +19,15 @@ class D(models.Model):                                                     # 院
 
 
 class T(models.Model):
-    gh = models.CharField(max_length=32, unique=True, primary_key=True)    # 工号
+    gh = models.CharField(max_length=32, unique=True, primary_key=True)    # 工号(主键)
     xm = models.CharField(max_length=32)                                   # 姓名
     xb = models.CharField(max_length=4)                                    # 性别
     csrq = models.DateField()                                              # 出生日期
     xl = models.CharField(max_length=32)                                   # 学历
     gz = models.FloatField()                                               # 工资
-    yxh = models.CharField(max_length=8)                                   # 院系号
+    yxh = models.ForeignKey('D', to_field='yxh',on_delete=models.CASCADE)      # 院系号(外键)
     pf = models.FloatField()                                               # 评分
+
 
 class C(models.Model):
     xq = models.CharField(max_length=32)                 # 学期
@@ -34,13 +35,14 @@ class C(models.Model):
     km = models.CharField(max_length=32)                 # 课名
     xf = models.IntegerField()                                             # 学分
     xs = models.IntegerField()                                             # 学时
-    yxh = models.CharField(max_length=8)                                   # 院系号
+    yxh = models.ForeignKey('D', to_field='yxh',on_delete=models.CASCADE)      # 院系号(外键)
     class Meta:
-        unique_together=("xq", "kh", "km")
+        unique_together=("xq", "kh")              # 联合主键(学期，课号)
+
 
 class O(models.Model):
     kh = models.CharField(max_length=32)                 # 课号
-    gh = models.CharField(max_length=32, unique=True)    # 工号
+    gh = models.ForeignKey('T', to_field='gh',on_delete=models.CASCADE)         # 工号(外键)
     sksj = models.CharField(max_length=32)               # 上课时间
     class Meta:
         unique_together = ("kh", "gh", "sksj")
@@ -48,9 +50,9 @@ class O(models.Model):
 class E(models.Model):
     xn = models.CharField(max_length=32)                 # 学年
     xq = models.CharField(max_length=32)                 # 学期
-    xh = models.CharField(max_length=32)                      # 学号
+    xh = models.ForeignKey('S', to_field='xh',on_delete=models.CASCADE)         # 学号(外键)
     kh = models.CharField(max_length=32)                 # 课号
-    gh = models.CharField(max_length=32, unique=True)    # 工号
+    gh = models.ForeignKey('T', to_field='gh',on_delete=models.CASCADE)         # 工号(外键)
     pscj = models.FloatField()                                             # 平时成绩
     kscj = models.FloatField()                                             # 考试成绩
     zpcj = models.FloatField()                                             # 总评成绩
